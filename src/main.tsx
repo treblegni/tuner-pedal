2233a40bdc708dcdc81a88eb43bd7c368cb935f8
treblegni <ingelbert.figueroa@gmail.com>
2023-05-21 23:34:17 -0700

    Update to dependecies and env

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { ReactNode, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode> as ReactNode
 )
